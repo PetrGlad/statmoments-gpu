056f49e74c07de636c7a2c2747a84046bc8d5a1d
Petr Gladkikh <petrglad@gmail.com>
2024-03-07 22:54:27 +0100

    Accumulate only necessary diagonal power elements

---
+++ gpu_acc.cu
@@ -0,0 +1,46 @@
+typedef double T;
+
+extern "C" __global__
+void pairs_update(const int n,
+                  const int max_single_p, const T *x_powers, T *x_powers_acc,
+                  const int cell_size, const int *acc_row_indexes, T *acc) {
+    // Using `_i` as shorthand for "index", `i` alone means row index.
+    const int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    printf("<%d (b=%d, t=%d)> # %d %d \n", tid, blockIdx.x, threadIdx.x, n, max_single_p);
+    const int cell_mat_size = cell_size * cell_size;
+    const int row_i = tid;
+    const int next_row_i = acc_row_indexes[row_i + 1];
+    int acc_i = acc_row_indexes[row_i];
+    int col_i = 0;
+    // TODO (scheduling) Updating a single row of the accumulator matrix for now.
+    //      Should be a contiguous range of rows.
+    const int powers_idx = row_i * cell_size;
+    // Update a diagonal cell.
+    for (int p = 0; p < max_single_p; p++) {
+        // We could sum the whole in one go, but then it would be a step that have to be scheduled separately.
+        // Alternatively this loop can be a part of powers calculation step.
+        // These are options to consider later (need some benchmarks to decide which one is better).
+        x_powers_acc[powers_idx * max_single_p + p] += x_powers[powers_idx * max_single_p + p];
+    }
+    // Calculate pair powers.
+    int powers_idx_b = 0;
+    printf("|before acc_i %d, next_row_i %d\n", acc_i, next_row_i);
+    // +1 to exclude diagonal (calculated separately).
+    const int powers_idx_a = powers_idx + 1;
+    // For each pair cell of the triangle matrix row.
+    for (; acc_i < next_row_i; col_i++) {
+        printf("|row acc_i %d, col_i %d\n", acc_i, col_i);
+        for (int pa = 0; pa < cell_size; pa++) {
+            for (int pb = 0; pb < cell_size; pb++) {
+                printf("|cell tid %d, acc_i %d, col_i %d, pa %d, pb %d, xp1 %f, xp2 %f\n",
+                       tid, acc_i, col_i, pa, pb, x_powers[powers_idx_a + pa], x_powers[powers_idx_b + pb]);
+                acc[acc_i] += x_powers[powers_idx_a + pa] * x_powers[powers_idx_b + pb];
+                acc_i++;
+            }
+        }
+
+        // FIXME 1.Reduce matrix acc size, 1. Update powers vector acc.
+
+        powers_idx_b += max_single_p;
+    }
+}
--- gpu_acc.py
+++ gpu_acc.py
@@ -16,7 +16,7 @@ m = (1, 2)  # The moment's power degrees that we need
 n = 3  # Number of scalar values in a measurement X
 # Max power degree to calc for each variable. max(m) for off-diagonal, sum(m) on the diagonal
 # Using sum to get all the values necessary for later calculations, as max(m) <= sum(m)
-max_p = sum(m)
+max_single_p = sum(m)
 
 ck_powers = cp.RawKernel(
     r"""
@@ -47,7 +47,7 @@ print(ck_powers.attributes)
 
 x = cp.arange(n, dtype=dtype) + 1
 print("> x=\n", x)
-x_powers = cp.zeros((n, max_p), dtype=dtype)
+x_powers = cp.zeros((n, max_single_p), dtype=dtype)
 
 block_size = min(ck_powers.max_threads_per_block, n)
 n_blocks = n // block_size
@@ -55,7 +55,7 @@ if n % block_size != 0:
     n_blocks += 1
 print(n_blocks, block_size)
 
-ck_powers((n_blocks,), (block_size,), (max_p, x, x_powers))  # grid (number of blocks), block and arguments
+ck_powers((n_blocks,), (block_size,), (max_single_p, x, x_powers))  # grid (number of blocks), block and arguments
 print("> x_powers=\n", x_powers)
 
 
@@ -66,52 +66,6 @@ def row_starts():
         i += p
         yield i
         p += 1
-
-
-ck_pairs_update = cp.RawKernel(
-    r"""
-  typedef double T;
-  
-  extern "C" __global__
-  void pairs_update(const int n,
-                    const int max_p, const T* x_powers, const T* x_powers_acc, 
-                    const int cell_size, const int* acc_row_indexes, T* acc) {
-    // Using `_i` as shorthand for "index", `i` alone means row index.
-    const int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    printf("<%d (b=%d, t=%d)> # %d %d \n", tid, blockIdx.x, threadIdx.x, n, p);
-    const int cell_mat_size = cell_size * cell_size;
-    const int row_i = tid;
-    const int next_row_i = acc_row_indexes[row_i + 1];
-    int acc_i = acc_row_indexes[row_i];
-    int col_i = 0;
-    // TODO (scheduling) Updating a single row of the accumulator matrix for now. 
-    //      Should be a contiguous range of rows
-    const int powers_idx_a = row_i * cell_size;
-    int powers_idx_b = 0;
-    // For each pair cell of the triangle matrix row.
-    printf("|before acc_i %d, next_row_i %d\n", acc_i, next_row_i);
-    for (; acc_i < next_row_i; col_i++) {
-      printf("|row acc_i %d, col_i %d\n", acc_i, col_i);
-      
-      // TODO Also update powers acc here
-      
-      for (int pa = 0; pa < cell_size; pa++) {
-        for (int pb = 0; pb < cell_size; pb++) {
-          printf("|cell tid %d, acc_i %d, col_i %d, pa %d, pb %d, xp1 %f, xp2 %f\n",
-                 tid, acc_i, col_i, pa, pb, x_powers[powers_idx_a + pa],  x_powers[powers_idx_b + pb]);
-          acc[acc_i] += x_powers[powers_idx_a + pa] * x_powers[powers_idx_b + pb];
-          acc_i++;
-        }
-      }
-      
-      FIXME 1.Reduce matrix acc size, 1. Update powers vector acc.
-      
-      powers_idx_b += max_p;
-    }
-  }
-  """,
-    "pairs_update",
-)
 
 
 def acc_to_2d_numpy(n_rows, cell_size, acc):
@@ -150,23 +104,28 @@ def acc_to_4d_numpy(n_rows, cell_size, acc):
 
 # ---------- X PAIRS ----------
 
+with open('gpu_acc.cu', 'r') as file:
+    ck_pairs_update_source = file.read()
+
+ck_pairs_update = cp.RawKernel(ck_pairs_update_source, "pairs_update")
+
 # n_acc_cols = n
 # n_acc_rows = math.ceil(n / 2)
 
 # Accumulates power sums of each variable
-x_powers_acc = cp.zeros((n, max_p), dtype=dtype)
+x_powers_acc = cp.zeros((n, max_single_p), dtype=dtype)
 
 starts = row_starts()
-cell_size = max(m)
-cell_matrix_size = cell_size * cell_size
+acc_cell_size = max(m)
+# Excluding diagonal in acc size since we're accumulating single var powers separately in x_powers_acc.
+acc_size = n - 1
+cell_matrix_size = acc_cell_size * acc_cell_size
 row_index = np.array([next(starts) * cell_matrix_size
-                      for _ in range(n + 1)], dtype=np.uint32)
+                      for _ in range(acc_size + 1)], dtype=np.uint32)
 print("> row_index=\n", row_index)
-assert row_index[-1] == math.floor((n + 1) * n / 2) * cell_matrix_size
 
 acc_row_indexes = cp.array(row_index, dtype=cp.uint32)
-# TODO Exclude diagonal acc size only needs to be n-1 since we're
-#  accumulating single var powers separately in x_powers_acc.
+# Excluding diagonal in acc size since we're accumulating single var powers separately in x_powers_acc.
 acc = cp.zeros((row_index[-1],), dtype=dtype)
 
 block_size = min(ck_pairs_update.max_threads_per_block, n)
@@ -174,13 +133,17 @@ n_blocks = n // block_size
 if n % block_size != 0:
     n_blocks += 1
 
-print(n_blocks, block_size)
-# grid (number of blocks), block size, followed by list user of arguments
+print(f"Scheduling tasks as {n_blocks} x {block_size}")
+# Parameters are: grid (number of blocks), block size, followed by list of the kernel arguments.
+assert x_powers.size == n * max_single_p
+assert x_powers.shape == x_powers_acc.shape
+assert row_index[-1] == math.floor((acc_size + 1) * acc_size / 2) * cell_matrix_size
+assert acc_cell_size * acc_cell_size ==  cell_matrix_size
 ck_pairs_update((n_blocks,), (block_size,),
-                (n, max_p, x_powers, x_powers_acc, cell_size, acc_row_indexes, acc))
+                (n, max_single_p, x_powers, x_powers_acc, acc_cell_size, acc_row_indexes, acc))
 
-print(f"n={n}, p={max_p}")
+print(f"n={n}, p={max_single_p}")
 print("> acc=\n", acc)
-print("> acc=\n", acc_to_2d_numpy(n, cell_size, acc))
+print("> acc=\n", acc_to_2d_numpy(acc_size, acc_cell_size, acc))
 
 print("\nDONE")
