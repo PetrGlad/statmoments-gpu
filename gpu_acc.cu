16e58163d2c4b14e6ce39da96527e413c6f809d5
Petr Gladkikh <petrglad@gmail.com>
2024-03-09 19:41:09 +0100

    Correct matrix accumulator update; add a test

--- gpu_acc.cu
+++ gpu_acc.cu
@@ -19,42 +19,69 @@ void powers(const int p, const T *x, T *y) {
 extern "C" __global__
 void pairs_update(const int n,
                   const int max_single_p, const T *x_powers, T *x_powers_acc,
-                  const int cell_size, const int *acc_row_indexes, T *acc) {
+                  const int pair_cell_size, const int *pair_acc_row_indexes, T *pair_acc) {
     // Using `_i` as shorthand for "index", `i` alone means row index.
     const int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    printf("<%d (b=%d, t=%d)> # %d %d \n", tid, blockIdx.x, threadIdx.x, n, max_single_p);
-    const int cell_mat_size = cell_size * cell_size;
+    printf("<%d (b=%d, t=%d)> n=%d max_single_p=%d pair_sell_size=%d\n", tid, blockIdx.x, threadIdx.x, n, max_single_p,
+           pair_cell_size);
     const int row_i = tid;
-    const int next_row_i = acc_row_indexes[row_i + 1];
-    int acc_i = acc_row_indexes[row_i];
-    int col_i = 0;
-    // TODO (scheduling) Updating a single row of the accumulator matrix for now.
-    //      Should be a contiguous range of rows.
-    const int powers_idx = row_i * cell_size;
+
     // Update a diagonal cell.
     for (int p = 0; p < max_single_p; p++) {
         // We could sum the whole in one go, but then it would be a step that have to be scheduled separately.
         // Alternatively this loop can be a part of powers calculation step.
         // These are options to consider later (need some benchmarks to decide which one is better).
-        x_powers_acc[powers_idx * max_single_p + p] += x_powers[powers_idx * max_single_p + p];
+        const int i = row_i * max_single_p + p;
+//        printf("|powers max_p=%d i=%d, x_powers[i]=%f\n", max_single_p, i, x_powers[i]);
+        x_powers_acc[i] += x_powers[i];
     }
+
     // Calculate pair powers.
-    int powers_idx_b = 0;
-    printf("|before acc_i %d, next_row_i %d\n", acc_i, next_row_i);
-    // +1 to exclude diagonal (calculated separately).
-    const int powers_idx_a = powers_idx + 1;
+    const int next_acc_row_i = pair_acc_row_indexes[row_i + 1];
+    int acc_i = pair_acc_row_indexes[row_i];
+    // TODO (scheduling) Updating a single row of the accumulator matrix for now.
+    //      Should be a contiguous range of rows.
+//    const int cell_mat_size = pair_cell_size * pair_cell_size;
+    printf("|before acc_i %d, next_acc_row_i %d\n", acc_i, next_acc_row_i);
+    // +1 to exclude diagonal (it is calculated separately).
+    const int powers_a_start_i = (row_i + 1) * max_single_p;
+    int powers_b_start_i = 0;
     // For each pair cell of the triangle matrix row.
     // Updating as lower triangle of cells (excluding diagonal).
-    for (; acc_i < next_row_i; col_i++) {
-        printf("|row acc_i %d, col_i %d\n", acc_i, col_i);
-        for (int pa = 0; pa < cell_size; pa++) {
-            for (int pb = 0; pb < cell_size; pb++) {
+    // (col_i - current cell column. It is here for debugging, can be removed)
+    for (int col_i = 0; acc_i < next_acc_row_i; col_i++) {
+//        printf("|row acc_i %d, col_i %d\n", acc_i, col_i);
+        for (int pa = 0; pa < pair_cell_size; pa++) {
+            for (int pb = 0; pb < pair_cell_size; pb++) {
                 printf("|cell tid %d, acc_i %d, col_i %d, pa %d, pb %d, xp1 %f, xp2 %f\n",
-                       tid, acc_i, col_i, pa, pb, x_powers[powers_idx_a + pa], x_powers[powers_idx_b + pb]);
-                acc[acc_i] += x_powers[powers_idx_a + pa] * x_powers[powers_idx_b + pb];
+                       tid, acc_i, col_i, pa, pb, x_powers[powers_a_start_i + pa], x_powers[powers_b_start_i + pb]);
+                pair_acc[acc_i] += x_powers[powers_a_start_i + pa] * x_powers[powers_b_start_i + pb];
                 acc_i++;
             }
         }
-        powers_idx_b += max_single_p;
+        powers_b_start_i += max_single_p;
     }
+}
+
+
+//extern "C" __global__ inline
+//void single_sum(const int max_single_p, T *x_powers_acc, const int i, const int p) {
+//    return x_powers_acc[max_single_p * i + p];
+//}
+
+extern "C" __global__
+void moments(const int m1, const int m2, const int n,
+             const int max_single_p, T *x_powers_acc,
+             const int pair_cell_size, const int *acc_row_indexes, T *pair_acc, T *moments) {
+    const int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    // Not implemented
+    printf("<<<moments(...) is not implemented>>>");
+    const int cell_mat_size = pair_cell_size * pair_cell_size;
+    const int row_i = tid;
+    const int powers_idx = row_i * pair_cell_size;
+    T ei = 0;
+//    for (int col_i = 0; acc_i < next_row_i; col_i++) {
+//        T ej = 0;
+//        moments[i * n + j]
+//    }
 }
--- gpu_acc.py
+++ gpu_acc.py
@@ -11,38 +11,9 @@ from sympy.plotting import plot
 
 print(cp.cuda.Device(), cp.cuda.device.get_compute_capability(), cp.cuda.get_current_stream())
 
-# Floats type to use. Update CUDA kernels when changing this.
-dtype = cp.float64
-m = (1, 2)  # The moment's power degrees that we need
-n = 3  # Number of scalar values in a measurement X
-# Max power degree to calc for each variable. max(m) for off-diagonal, sum(m) on the diagonal
-# Using sum to get all the values necessary for later calculations, as max(m) <= sum(m)
-max_single_p = sum(m)
 
-with open('gpu_acc.cu', 'r') as f:
-    gpu_acc_source = f.read()
-gpu_acc_module = cp.RawModule(code=gpu_acc_source)
-
-# ---------- X POWERS ----------
-
-ck_powers = gpu_acc_module.get_function('powers')
-
-print(ck_powers.attributes)
-x = cp.arange(n, dtype=dtype) + 1
-print("> x=\n", x)
-x_powers = cp.zeros((n, max_single_p), dtype=dtype)
-
-block_size = min(ck_powers.max_threads_per_block, n)
-n_blocks = n // block_size
-if n % block_size != 0:
-    n_blocks += 1
-print(n_blocks, block_size)
-
-ck_powers((n_blocks,), (block_size,), (max_single_p, x, x_powers))  # grid (number of blocks), block and arguments
-print("> x_powers=\n", x_powers)
-
-
-def row_starts():
+def triangle_row_starts():
+    """Starting indices of a dense lower triangle matrix."""
     p = 0
     i = 0
     while True:
@@ -55,7 +26,7 @@ def acc_to_2d_numpy(n_rows, cell_size, acc):
     """Convert in-GPU accumulator representation to 2d numpy array.
        For diagnostics and GPU->host export.
        This may produce output that is easier to inspect than one of acc_to_4d_numpy."""
-    out = np.zeros((n_rows * cell_size, n_rows * cell_size), dtype=dtype)
+    out = np.zeros((n_rows * cell_size, n_rows * cell_size), dtype=DTYPE)
     row_len = 1
     idx = 0
     for i in range(n_rows):
@@ -72,7 +43,7 @@ def acc_to_4d_numpy(n_rows, cell_size, acc):
     """Convert in-GPU accumulator representation to a logically laid out numpy array.
        For diagnostics and GPU->host export.
        first 2 indexes select variable pair, second 2 indexes select powers of sums."""
-    out = np.zeros((n_rows, n_rows, cell_size, cell_size), dtype=dtype)
+    out = np.zeros((n_rows, n_rows, cell_size, cell_size), dtype=DTYPE)
     row_len = 1
     idx = 0
     for i in range(n_rows):
@@ -85,45 +56,140 @@ def acc_to_4d_numpy(n_rows, cell_size, acc):
     return out
 
 
-# ---------- X PAIRS ----------
+# Floats type to use. Update CUDA kernels when changing this.
+DTYPE = cp.float64
 
-ck_pairs_update = gpu_acc_module.get_function('pairs_update')
+with open('gpu_acc.cu', 'r') as f:
+    gpu_acc_source = f.read()
+GPU_ACC_MODULE = cp.RawModule(code=gpu_acc_source)
 
-# n_acc_cols = n
-# n_acc_rows = math.ceil(n / 2)
+CK_POWERS = GPU_ACC_MODULE.get_function('powers')
+print("ck_powers:", CK_POWERS.attributes)
 
-# Accumulates power sums of each variable
-x_powers_acc = cp.zeros((n, max_single_p), dtype=dtype)
+CK_PAIRS_UPDATE = GPU_ACC_MODULE.get_function('pairs_update')
+print("ck_pairs_update:", CK_PAIRS_UPDATE.attributes)
 
-starts = row_starts()
-acc_cell_size = max(m)
-# Excluding diagonal in acc size since we're accumulating single var powers separately in x_powers_acc.
-acc_size = n - 1
-cell_matrix_size = acc_cell_size * acc_cell_size
-row_index = np.array([next(starts) * cell_matrix_size
-                      for _ in range(acc_size + 1)], dtype=np.uint32)
-print("> row_index=\n", row_index)
+CK_MOMENTS = GPU_ACC_MODULE.get_function('moments')
 
-acc_row_indexes = cp.array(row_index, dtype=cp.uint32)
-# Excluding diagonal in acc size since we're accumulating single var powers separately in x_powers_acc.
-acc = cp.zeros((row_index[-1],), dtype=dtype)
 
-block_size = min(ck_pairs_update.max_threads_per_block, n)
-n_blocks = n // block_size
-if n % block_size != 0:
-    n_blocks += 1
+class MomentsGpu:
+    def __init__(self, m: (int, int), n_x: int):
+        # Required max moment's power degrees
+        self.m = m
+        # Number of scalar measurements in a sample X
+        self.n_x = n_x
+        # Max power degree to calc for each variable. max(m) for off-diagonal, sum(m) on the diagonal
+        # Using sum to get all the values necessary for later calculations, as max(m) <= sum(m)
+        self.max_single_p = sum(m)
+        # Sums of single measurement powers
+        self.x_powers = cp.zeros((n_x, self.max_single_p), dtype=DTYPE)
+        # Accumulates power sums of each variable
+        self.x_powers_acc = cp.zeros((n_x, self.max_single_p), dtype=DTYPE)
+        assert self.x_powers.size == self.n_x * self.max_single_p
+        assert self.x_powers.shape == self.x_powers_acc.shape
 
-print(f"Scheduling tasks as {n_blocks} x {block_size}")
-# Parameters are: grid (number of blocks), block size, followed by list of the kernel arguments.
-assert x_powers.size == n * max_single_p
-assert x_powers.shape == x_powers_acc.shape
-assert row_index[-1] == math.floor((acc_size + 1) * acc_size / 2) * cell_matrix_size
-assert acc_cell_size * acc_cell_size == cell_matrix_size
-ck_pairs_update((n_blocks,), (block_size,),
-                (n, max_single_p, x_powers, x_powers_acc, acc_cell_size, acc_row_indexes, acc))
+        self.pairs_acc_cell_size = max(m)
+        # Side size of the pairs acc triangle matrix.
+        # Excluding diagonal in acc size since we're accumulating single var powers separately in x_powers_acc.
+        self.pairs_acc_size = n_x - 1
+        # In theory, if we were only requiring one-way moments (e.g. E(XaXbXb) for only a <= b)
+        #   then the matrix may be reduced to shape self.m
+        self.cell_matrix_size = self.pairs_acc_cell_size ** 2
+        row_starts = triangle_row_starts()
+        row_index = np.array([next(row_starts) * self.cell_matrix_size
+                              for _ in range(self.pairs_acc_size + 1)], dtype=np.uint32)
+        print("> row_index=\n", row_index)
 
-print(f"n={n}, p={max_single_p}")
-print("> acc=\n", acc)
-print("> acc=\n", acc_to_2d_numpy(acc_size, acc_cell_size, acc))
+        pairs_acc_length = row_index[-1]
+        assert pairs_acc_length == math.floor(
+            (self.pairs_acc_size + 1) * self.pairs_acc_size / 2) * self.cell_matrix_size
+        self.pairs_acc_row_indexes = cp.array(row_index, dtype=cp.uint32)
+        self.pairs_acc = cp.zeros((pairs_acc_length,), dtype=DTYPE)
 
+        # Number of processed samples
+        self.n_updates = 0
+
+    def update(self, x):
+        # Just curious, if it holds
+        assert CK_POWERS.max_threads_per_block == CK_PAIRS_UPDATE.max_threads_per_block
+        assert x.size == self.n_x
+
+        # TODO DRY Scheduling sizes calculation.
+        block_size = min(CK_POWERS.max_threads_per_block, self.n_x)
+        n_blocks = self.n_x // block_size
+        if self.n_x % block_size != 0:
+            n_blocks += 1
+        print(n_blocks, block_size)
+
+        CK_POWERS((n_blocks,), (block_size,),
+                  (self.max_single_p, x, self.x_powers))  # grid (number of blocks), block and arguments
+        print("> x_powers=\n", self.x_powers)
+
+        # TODO DRY Scheduling sizes calculation.
+        block_size = min(CK_PAIRS_UPDATE.max_threads_per_block, self.n_x)
+        n_blocks = self.n_x // block_size
+        if self.n_x % block_size != 0:
+            n_blocks += 1
+
+        print(f"Scheduling tasks as {n_blocks} x {block_size}")
+        # Parameters are: grid (number of blocks), block size, followed by list of the kernel arguments.
+        CK_PAIRS_UPDATE((n_blocks,), (block_size,),
+                        (self.n_x, self.max_single_p, self.x_powers, self.x_powers_acc,
+                         self.pairs_acc_cell_size, self.pairs_acc_row_indexes, self.pairs_acc))
+
+    def moments(self, m):
+        assert m[0] <= self.m[0] and m[1] <= self.m[1]
+
+        # TODO Implement generic case.
+        assert m == (1, 1)
+        ms = cp.array(self.n_x)
+
+        # TODO DRY Scheduling sizes calculation.
+        block_size = min(CK_PAIRS_UPDATE.max_threads_per_block, nx)
+        n_blocks = self.n_x // block_size
+        if self.n_x % block_size != 0:
+            n_blocks += 1
+
+        CK_MOMENTS((n_blocks,), (block_size,), (m[0], m[1], ms))
+
+        return ms
+
+
+def test_acc_update_1():
+    nx = 3
+    acc = MomentsGpu((1, 2), nx)
+    x = cp.arange(nx, dtype=DTYPE) + 1
+    acc.update(x)
+
+    expected_pairs = np.array([[2., 2., 0., 0.],
+                               [4., 4., 0., 0.],
+                               [3., 3., 6., 12.],
+                               [9., 9., 18., 36.]])
+    actual_pairs = acc_to_2d_numpy(acc.pairs_acc_size, acc.pairs_acc_cell_size, acc.pairs_acc)
+    assert np.array_equal(expected_pairs, actual_pairs)
+
+    expected_powers = np.array([[1., 1., 1.],
+                                [2., 4., 8.],
+                                [3., 9., 27.]])
+    assert np.array_equal(expected_powers, cp.asnumpy(acc.x_powers_acc))
+
+
+def lab():
+    # https://docs.cupy.dev/en/stable/user_guide/performance.html
+    nx = 3
+    m = 1, 2
+    acc = MomentsGpu(m, nx)
+    x = cp.arange(nx, dtype=DTYPE) + 1
+    print("> x=\n", x)
+    acc.update(x)
+    print(f"n={nx}, p={acc.max_single_p}")
+    print("> acc=\n", acc.pairs_acc)
+    print("> acc=\n", acc_to_2d_numpy(acc.pairs_acc_size, acc.pairs_acc_cell_size, acc.pairs_acc))
+
+
+print("\n=== TESTS ===")
+test_acc_update_1()
+print("TESTS OK\n")
+
+lab()
 print("\nDONE")
